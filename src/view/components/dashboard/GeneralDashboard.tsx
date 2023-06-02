dd8a3c1dd2d450e1c597a258532a84cdb352e329
leofuna <leo.funa@gmail.com>
2023-06-02 12:04:14 -0300

    fix: using income section on general dashbord

--- src/view/components/dashboard/GeneralDashboard.tsx
+++ src/view/components/dashboard/GeneralDashboard.tsx
@@ -1,15 +1,15 @@
 import { CreditCard } from "./CreditCard"
+import { IncomeSection } from "./IncomeSection"
 
 export function GeneralDashboard() {
   return (
     <div style={{ height: '95%' }} className="flex items-center justify-center bg-white w-full rounded-3xl space-x-5">
       <div className="flex flex-col items-center w-8/12 h-full">
-        <div className="flex justify-center bg-white w-11/12 h-3/6 rounded-3xl space-x-5">
+        <div className="flex justify-center bg-white w-full h-3/6 space-x-4 rounded-3xl">
           {/* @ts-expect-error Async Server Components */}
           <CreditCard />
-          <div className="bg-blue w-5/12 h-full">
-
-          </div>
+          {/* @ts-expect-error Async Server Components */}
+          <IncomeSection />
         </div>
         <div className="bg-white w-11/12 h-3/6 rounded-3xl">
           Transaçoes
