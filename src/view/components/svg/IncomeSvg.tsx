40c2507bbea0d4837c9060e547c2c4adc58c3d9f
leofuna <leo.funa@gmail.com>
2023-06-03 17:28:23 -0300

    fix: getting custom className from props for incomeSvg

--- src/view/components/svg/IncomeSvg.tsx
+++ src/view/components/svg/IncomeSvg.tsx
@@ -1,8 +1,8 @@
-export function IncomeSvg() {
+export function IncomeSvg({ className }: { className: string }) {
   return (
     <svg
       aria-hidden="true"
-      className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75"
+      className={className}
       fill="currentColor"
       viewBox="0 0 20 20"
       xmlns="http://www.w3.org/2000/svg"
