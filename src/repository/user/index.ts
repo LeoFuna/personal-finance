d28110414abbe42013c09b7a8e232e302dfbe665
leofuna <leo.funa@gmail.com>
2023-06-07 17:06:24 -0300

    fix: typing UserRepository class return

--- src/repository/user/index.ts
+++ src/repository/user/index.ts
@@ -1,10 +1,11 @@
 import { Repository } from "@/interfaces/repository";
+import { UserOnApp } from "@/interfaces/user";
 
 type UserCreatePayload = {
   currentDate: Date;
 }
 
 export abstract class UserRepository implements Repository {
-  abstract create(email: string, payload: UserCreatePayload): Promise<any>;
-  abstract getUnique(email: string): Promise<any>;
+  abstract create(email: string, payload: UserCreatePayload): Promise<UserOnApp>;
+  abstract getUnique(email: string): Promise<UserOnApp>;
 }
