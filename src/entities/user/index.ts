eb598cdcd9ab04a8b883998fdba5324eb6fd555a
leofuna <leo.funa@gmail.com>
2023-06-07 16:57:37 -0300

    feat: user entity

---
+++ src/entities/user/index.ts
@@ -0,0 +1,19 @@
+import { UserOnApp } from "@/interfaces/user";
+
+export class User implements UserOnApp {
+  email: string;
+  createdAt: Date;
+
+  constructor({ email, createdAt }: UserOnApp) {
+    this.email = email;
+    this.createdAt = createdAt
+  }
+
+  getEmail() {
+    return this.email;
+  }
+
+  getCreatedAt() {
+    return this.createdAt;
+  }
+}
