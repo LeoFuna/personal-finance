32e0df4b2a33c593d514a1d430f74e4ce2029b50
leofuna <leo.funa@gmail.com>
2023-06-07 16:55:34 -0300

    feat: user service implements service

---
+++ src/services/user/index.ts
@@ -0,0 +1,17 @@
+import { Service } from "@/interfaces/service";
+import { UserOnApp } from "@/interfaces/user";
+import { UserRepository } from "@/repository/user";
+
+export class UserService implements Service {
+  constructor(private userRepository: UserRepository) {}
+
+  async getUnique(email: string): Promise<UserOnApp> {
+    // Validaçao aqui
+    return await this.userRepository.getUnique(email);
+  };
+
+  async create(email: string, payload: { currentDate: Date }): Promise<UserOnApp> {
+    // Validaçao aqui
+    return await this.userRepository.create(email, payload)
+  };
+}
