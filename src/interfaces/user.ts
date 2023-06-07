4c6e7bcb801a53466972e61b8ba528d262d1d066
leofuna <leo.funa@gmail.com>
2023-06-07 16:57:58 -0300

    fix: removing unecessary interface

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -2,8 +2,3 @@ export interface UserOnApp {
   email: string;
   createdAt: Date;
 }
-
-export interface UserRepository {
-  create: (email: string) => Promise<UserOnApp>;
-  getUser: (email: string) => Promise<UserOnApp>;
-}
