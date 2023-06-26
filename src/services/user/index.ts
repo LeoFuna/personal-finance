71db334fb930dbef6db1e2d27721e5f4e1dcc23a
leofuna <leo.funa@gmail.com>
2023-06-26 14:56:36 -0300

    fix: some basic validations on getUnique and create UserServices

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -3,10 +3,12 @@ import { UserOnApp } from '@/interfaces/user';
 import { UserRepository } from '@/repository/user';
 
 export class UserService implements Service {
-  constructor(private userRepository: UserRepository) {}
+  constructor(private userRepository: UserRepository) {
+    this.userRepository = userRepository;
+  }
 
   async getUnique(email: string): Promise<UserOnApp> {
-    // Validaçao aqui
+    if (!email || !email.length) throw new Error('Invalid data');
     return await this.userRepository.getUnique(email);
   }
 
@@ -14,7 +16,10 @@ export class UserService implements Service {
     email: string,
     payload: { currentDate: Date }
   ): Promise<UserOnApp> {
-    // Validaçao aqui
+    if (!email || !email.length) throw new Error('Invalid data');
+    if (!payload?.currentDate || !(payload.currentDate instanceof Date)) {
+      throw new Error('Invalid data');
+    }
     return await this.userRepository.create(email, payload);
   }
 }
