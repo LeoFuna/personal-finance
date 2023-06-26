a18e59551fc9b856ac47bd25a43cf53f7f886655
leofuna <leo.funa@gmail.com>
2023-06-26 14:55:42 -0300

    build: lint disable on interfaces

--- src/interfaces/repository.ts
+++ src/interfaces/repository.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 export interface Repository {
   create: (uniqueKey: string, payload: any) => Promise<any>;
   getUnique: (uniqueKey: string) => Promise<any>;
--- src/interfaces/service.ts
+++ src/interfaces/service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 export interface Service {
   create: (uniqueKey: string, payload: any) => Promise<any>;
   getUnique: (uniqueKey: string) => Promise<any>;
