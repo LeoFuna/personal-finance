33d0b53a21ef3e91ebb0d1a2023b10ae1b63959d
leofuna <leo.funa@gmail.com>
2023-06-27 15:50:58 -0300

    fix: account svg props

--- src/view/components/svg/AccountSvg.tsx
+++ src/view/components/svg/AccountSvg.tsx
@@ -10,7 +10,7 @@ export function AccountSvg() {
       <path
         fillRule="evenodd"
         d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       ></path>
     </svg>
   );
