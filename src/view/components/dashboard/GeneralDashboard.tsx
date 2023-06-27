b96045a14acfb1f05b69fa27a70ac2276a313670
leofuna <leo.funa@gmail.com>
2023-06-27 15:51:49 -0300

    feat: some adjusts on general dashboard component

--- src/view/components/dashboard/GeneralDashboard.tsx
+++ src/view/components/dashboard/GeneralDashboard.tsx
@@ -1,5 +1,6 @@
 import { CreditCard } from './CreditCard';
 import { IncomeSection } from './IncomeSection';
+import Transactions from './Transactions';
 
 export function GeneralDashboard() {
   return (
@@ -14,9 +15,26 @@ export function GeneralDashboard() {
           {/* @ts-expect-error Async Server Components */}
           <IncomeSection />
         </div>
-        <div className="bg-white w-11/12 h-3/6 rounded-3xl">Transaçoes</div>
+        <div className="bg-white w-11/12 h-3/6 rounded-3xl">
+          <h1 className="section-title">Transações Recentes</h1>
+          <Transactions />
+        </div>
       </div>
-      <div className="bg-blue w-3/12 h-full"></div>
+      <div className="w-3/12 h-full flex justify-center items-center">
+        <div className="bg-gray w-full h-5/6 rounded-2xl">
+          <h1>Saldo do Mes</h1>
+          <p>R$ 250,00</p>
+          <h1>Saldo Custos Variáveis</h1>
+          <p>R$ - 50,00</p>
+          <h1>Custos Variáveis</h1>
+          {/* Esse caso aqui provavelmente teremos que pensa rnuma paginaçao para trazer os dados */}
+          {/* Caso haja muitos custos variáveis */}
+          <div className="flex">
+            <p>Previsto: X</p>
+            <p>Realizado: Y</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
